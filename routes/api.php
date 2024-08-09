868b84d860abac405175e00d1b8845439ffbffcb
Leonardo Lima <llimaesilva7@gmail.com>
2024-08-09 11:09:07 -0300

    Agrupa rotas

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use App\Http\Controllers\TodoColorController;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\TodoFavoriteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('todo', TodoController::class);
-Route::patch('todo/{todo}/color', [TodoColorController::class, 'update']);
+Route::group(['prefix' => 'todo'], function(){
+    Route::patch('{todo}/color', [TodoColorController::class, 'update']);
+    Route::patch('{todo}/favorite', [TodoFavoriteController::class, 'update']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
