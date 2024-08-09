e047c395d9ca9e414e940dd6238d900502999a45
Leonardo Lima <llimaesilva7@gmail.com>
2024-08-09 15:58:13 -0300

    Refatorando listagem de todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\FilterTodos;
 use App\Http\Requests\TodoRequest;
 use App\Http\Resources\TodoResource;
 use App\Models\Todo;
@@ -12,14 +13,7 @@ class TodoController extends Controller
     public function index(Request $request)
     {
         try {
-            $todos = Todo::query()
-            ->when($request->favorite === 'true', function($todos) {
-                return $todos->where('is_favorite', 1);
-            })
-            ->when($request->favorite === 'false', function($todos) {
-                return $todos->where('is_favorite', 0);
-            })
-            ->paginate();
+            $todos = FilterTodos::run($request->favorite)->paginate();
             return TodoResource::collection($todos);
         } catch (\Exception $e) {
             return response()->json(['data' => ['error' => $e->getMessage()]], 500);
